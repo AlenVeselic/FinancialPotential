4cf31e90757a90b35ccb294de321851a31554e5e
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2021-03-17 14:50:45 +0100

    Project initiated

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/**
+ * Description
+ * @authors Your Name (you@example.org)
+ * @date    2021-03-16 12:15:42
+ * @version 1.0.0
+ */
+
+
+
+document.getElementById('date').innerHTML = new Date().toDateString()
